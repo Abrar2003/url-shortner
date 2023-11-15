e9d70a54be76a764d07cb713674d3cfae7200636
Abrar2003 <spartanabrar@gmail.com>
2023-11-15 10:59:15 +0530

    test route added

---
+++ backend/controllers/url.controller.js
@@ -0,0 +1,5 @@
+const test_url_controller = ( req, res ) => {
+    res.send("Hello URL");
+}
+
+module.exports = { test_url_controller };
--- backend/routes/url.route.js
+++ backend/routes/url.route.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const URL = require("../models/url.model");
+const { test_url_controller } = require("../controllers/url.controller");
 
 const Router = express.Router();
 
-Router.get("/", (req, res) => {
-    res.send("Hello URL")
-});
+Router.get("/", test_url_controller);
 
 module.exports = Router;
