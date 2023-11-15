1ff3db4c5be12b4c54d8af6b0f026b6117ad01cc
Shahid321fw11 <sidansari20@gmail.com>
2023-11-15 16:09:37 +0530

    style : output box style changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,9 @@ const Home = () => {
     // Handle the logic for generating output based on the form data
     // You can use the formData to generate the output as per your requirements
     // For now, let's just set it to a string representation of the form data
-    setOutput(JSON.stringify(formData, null, 2));
+    const { originalUrl } = formData;
+
+    setOutput(JSON.stringify({ originalUrl }, null, 2));
   };
 
   const scrollToTop = () => {
@@ -20,13 +22,12 @@ const Home = () => {
     <div className="container mx-auto p-8">
       <h1 className="text-4xl font-bold mb-8 text-center">Home Page</h1>
       <Form onGenerate={handleGenerate} />
-      <div className="mt-8">
-        <pre className="bg-gray-200 p-4 rounded">
+      <div className="mx-auto mt-8 p-4 bg-white rounded-lg shadow-lg">
+        <pre className="bg-gray-200 p-4 rounded overflow-x-auto">
           <h2 className="text-2xl font-bold mb-4">Output:</h2>
           {output}
         </pre>
       </div>
-
       {/* Integrate the StickyButton component */}
       <StickyButton onClick={scrollToTop} label="All URL's" />
     </div>
